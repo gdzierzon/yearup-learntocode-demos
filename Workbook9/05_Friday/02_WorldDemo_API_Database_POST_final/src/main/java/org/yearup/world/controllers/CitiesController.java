a02c85de2121e657c1d8ba4ef324716ddc4fe4f7
Gregor Dzierzon <gregor@fullstackdevs.net>
2023-06-23 09:11:29 -0600

    adde POST demo

---
+++ Workbook9/05_Friday/02_WorldDemo_API_Database_POST_final/src/main/java/org/yearup/world/controllers/CitiesController.java
@@ -0,0 +1,57 @@
+package org.yearup.world.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.yearup.world.data.CityDao;
+import org.yearup.world.models.City;
+
+import java.util.List;
+
+@RestController
+public class CitiesController
+{
+    private CityDao dao;
+
+    @Autowired
+    public CitiesController(CityDao dao)
+    {
+        this.dao = dao;
+    }
+
+
+
+    // http://localhost:8080/cities  == default to USA
+    // http://localhost:8080/cities?country=DEU
+    @GetMapping(path="cities")
+    public List<City> getAll(@RequestParam(defaultValue = "USA") String country)
+    {
+        return dao.searchByCountry(country);
+    }
+
+
+
+    @GetMapping(path = "cities/{id}")
+    public City getById(@PathVariable int id)
+    {
+        var city = dao.getById(id);
+
+        if(city == null)
+        {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "The id: " + id + " does not exist.");
+        }
+
+        return city;
+    }
+
+    // add = POST
+//    @RequestMapping(path="cities", method = RequestMethod.POST)
+    @PostMapping(path="cities")
+    public City addCity(@RequestBody City city)
+    {
+        var newCity = dao.add(city);
+        return newCity;
+    }
+
+}
---
+++ Workbook9/05_Friday/02_WorldDemo_API_Database_POST_final/src/test/java/org/yearup/world/WorldDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yearup.world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
